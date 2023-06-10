19c669f9816a0c7de891e481aed1f350ad025da8
Gergely Petrik <gpetrik@gmail.com>
2023-06-10 20:27:43 +0200

    opencv4 support IplImage* -> cv::Mat where needed or easy to change

---
+++ cv.h
@@ -0,0 +1,8 @@
+#pragma once
+#define CV__ENABLE_C_API_CTORS
+#include <opencv2/opencv.hpp>
+
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/videoio/videoio_c.h>
+#include <opencv2/core/core_c.h>
+
